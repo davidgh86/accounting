a9f1ac05490c6f0dca4b1d2707de477e67473218
raulhsj <Raul.Hernandez@ust-global.com>
2018-08-06 09:29:13 +0200

    @ApiModelProperty added for QA better guidance

--- src/main/java/com/ust/calc/calculadora/api/resources/Contract.java
+++ src/main/java/com/ust/calc/calculadora/api/resources/Contract.java
@@ -2,6 +2,7 @@ package com.ust.calc.calculadora.api.resources;
 
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -12,21 +13,29 @@ import lombok.*;
 public class Contract {
 
 	@NotNull(message="The category is required")
+	@ApiModelProperty(required=true)
 	private String category;
 	@NotNull(message="The contract type is required")
+	@ApiModelProperty(required=true)
 	private String contractType;
 	@NotNull(message="The start date is required")
+	@ApiModelProperty(required=true)
 	private String startDate;
 	@NotNull(message="The salary is required")
+	@ApiModelProperty(required=true)
 	private String salary;
 	@NotNull(message="The currency is required")
+	@ApiModelProperty(required=true)
 	private String currency;
 	private String endDate;
 	@NotNull(message="The insurance number is required")
+	@ApiModelProperty(required=true)
 	private String insuranceNumber;
 	@NotNull(message="The civil status is required")
+	@ApiModelProperty(required=true)
 	private String civilStatus;
 	@NotNull(message="The current account is required")
+	@ApiModelProperty(required=true)
 	private String currentAccount;
 	
 }
--- src/main/java/com/ust/calc/calculadora/api/resources/Employee.java
+++ src/main/java/com/ust/calc/calculadora/api/resources/Employee.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,16 +22,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Api
 public class Employee {
 
 	@NotNull(message="The name is required")
 	@Size(min = 3, message="The name must be longer")
+	@ApiModelProperty(required=true)
     private String name;
 
     private String address;
 	
 	@NotNull(message="The e-mail is required")
 	@Pattern(regexp=".+@.+\\.[a-z]+", message="E-mail format not valid")
+	@ApiModelProperty(required=true)
     private String email;
 	
 	@NotNull(message="The DNI is required")
@@ -37,17 +42,21 @@ public class Employee {
 	    @Pattern(regexp = "((([X-Z])|([LM])){1}([-]?)((\\d){7})([-]?)([A-Z]{1}))|((\\d{8})([-]?)([A-Z]))", message = "The DNI must contain one digit."),
 //	    @Pattern(regexp = "[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]", message = "Password must contain one lowercase letter."),	    @Pattern(regexp = "(?=.*[A-Z])", message = "Password must contain one uppercase letter."),
 	})
+	@ApiModelProperty(required=true)
     private String dni;
 	
 	@NotNull(message="The user date of birth is required")
+	@ApiModelProperty(required=true)
     private Date birthDate;
     private String phoneNumber;
     private String sex;
     private String id;
     
 	@NotNull(message="The contract is required")
+	@ApiModelProperty(required=true)
     private Contract contract;
 	
+	@ApiModelProperty(value="cadastreRef",allowEmptyValue=false,required=false)
 	@Pattern(regexp="^([a-zA-Z0-9_.-]{14})$|^([a-zA-Z0-9_.-]{20})$", message="cadastreRef size must be 14 or 20 characters")
 	private String cadastreRef;
 
